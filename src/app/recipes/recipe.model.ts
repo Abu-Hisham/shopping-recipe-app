9b8cdbb5ab650a21a271264bfd4988127c94dbd5
Abdulaziz <abdulmoha786@gmail.com>
2018-10-26 16:05:09 +0300

    Add Ingredients Display Feature

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,17 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name, description, imagePath){
+    constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]){
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
+
     }
 }
